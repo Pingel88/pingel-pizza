341bd52b74fd4f0420a5fe7df938732bbfa8e2e6
Michael Pingel <mdpingel@gmail.com>
2021-01-29 09:10:01 -0800

    PASS spec #2, add Pizza.prototype.price to calculate pizza cost

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,21 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.price = function() {
+  let pizzaPrice = 0
+  let toppings = this.toppings
+  if (this.size === "small") {
+    pizzaPrice += 5;
+  } else if (this.size === "medium") {
+    pizzaPrice += 10;
+  } else if (this.size === "large") {
+    pizzaPrice += 15;
+  }
+  pizzaPrice += toppings.length;
+  return pizzaPrice;
+}
+
+pizza1 = new Pizza("small", ["olives", "sausage", "salami"]);
+
+console.log("Your pizza will cost $" + pizza1.price());
