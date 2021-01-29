bac60637441720cb3a2e910248904bf926ffdf22
Michael Pingel <mdpingel@gmail.com>
2021-01-29 14:10:49 -0800

    add javascript and css to get a user interface functioning correctly, playing with css flexbox

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,13 @@ function Pizza(size, toppings) {
 }
 
 Pizza.prototype.price = function() {
-  console.log("started price prototype");
   let pizzaPrice = 0;
   let toppings = this.toppings;
   if (this.size === "small") {
-    console.log("this is a small pizza");
     pizzaPrice += 5;
   } else if (this.size === "medium") {
-    console.log("this is a medium pizza");
     pizzaPrice += 10;
   } else if (this.size === "large") {
-    console.log("this is a large pizza");
     pizzaPrice += 15;
   }
   if (toppings != undefined) {
@@ -27,26 +23,15 @@ function undefinedFilter(array) {
   filteredArray = array.filter(function (element) {
     return element != null;
   });
-  console.log("line 32: " + array);
-  console.log("line 33: " + typeof(array));
-  console.log("line 34: " + filteredArray);
-  console.log("line 35: " + typeof(filteredArray));
   return filteredArray;
 };
 
 $(document).ready(function() {
-  $("form#pizza").submit(function(event) {
+  $("form.pizza").submit(function(event) {
     event.preventDefault();
     const pizzaSize = $("input:radio[name=pizza-size]:checked").val();
     const pizzaToppings = toppingsAssembler();
     const clientPizza = new Pizza(pizzaSize, pizzaToppings);
-    const clientPizza2 = new Pizza(pizzaSize, pizzaToppings);
-    console.log(clientPizza2);
-    console.log("line 46: " + pizzaSize);
-    console.log("line 47: " + pizzaToppings);
-    console.log("line 48: " + typeof(clientPizza.size));
-    console.log("line 49: " + typeof(clientPizza.toppings));
-    console.log("line 50: " + typeof(pizzaToppings));
     $("#total-cost").show();
     $("#pizza-total").html(clientPizza.price());
   });
