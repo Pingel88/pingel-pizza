e0f016e872943603f61c763ca6def055059034cc
Michael Pingel <mdpingel@gmail.com>
2021-01-30 22:22:31 -0800

    add working cart total that updates when pizzas get added, add collapsible pizza information when the pizza name is clicked in the cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
-function Pizza(size, toppings) {
+function Pizza(name, size, sauce, cheese, toppings) {
+  this.name = name;
+  this.price = 0;
   this.size = size;
+  this.sauce = sauce;
+  this.cheese = cheese;
   this.toppings = toppings;
-}
+};
 
-Pizza.prototype.price = function() {
-  let pizzaPrice = 0;
+Pizza.prototype.addPrice = function() {
   let toppings = this.toppings;
   if (this.size === "small") {
-    pizzaPrice += 5;
+    this.price += 5;
+    if (toppings != undefined) {
+      this.price += toppings.length;
+    }
   } else if (this.size === "medium") {
-    pizzaPrice += 10;
+    this.price += 10;
+    if (toppings != undefined) {
+      this.price += toppings.length * 2;
+    }
   } else if (this.size === "large") {
-    pizzaPrice += 15;
+    this.price += 15;
+    if (toppings != undefined) {
+      this.price += toppings.length * 3;
+    }
   }
-  if (toppings != undefined) {
-    pizzaPrice += toppings.length;
-  }
-  return pizzaPrice;
 };
 
 function undefinedFilter(array) {
@@ -26,55 +34,102 @@ function undefinedFilter(array) {
   return filteredArray;
 };
 
+Pizza.prototype.updateCartTotal = function(cartTotal) {
+  let newCartTotal = this.price + cartTotal;
+  return newCartTotal;
+};
+
+Pizza.prototype.addToCart = function() {
+  const name = this.name;
+  const idName = name.split(" ").join("");
+  $('#pizzas').append("<h5 id='pizza-name-in-cart-" + idName + "'>" + this.name + " - $" + this.price + "</h5>");
+  $('#pizzas').append("<p id='pizza-in-cart-" + idName + "'><span class='pizza-key'>Size: </span>" + this.size + "</br><span class='pizza-key'>Sauce: </span>" + this.sauce + "<br><span class='pizza-key'>Cheese: </span>" + this.cheese + "<br><span class='pizza-key'>Toppings: </span>" + this.toppings + "</p>");
+};
+
+Pizza.prototype.attachListeners = function() {
+  const name = this.name;
+  const idName = name.split(" ").join("");
+  const headerId = "#pizza-name-in-cart-" + idName;
+  const paragraphId = "#pizza-in-cart-" + idName;
+  $("#pizzas").on("click", headerId, function() {
+    console.log(name);
+    $(paragraphId).slideToggle();
+  });
+};
+
 $(document).ready(function() {
+  let cart = 0;
+
   $("#build-pizza").click(function() {
     $("#heading-text").hide();
-    $('#heading').slideUp();
+    $("#heading").slideUp();
     $("#build-pizza").slideUp();
     $("#pizza-menu").slideDown();
     $("#main-interface").slideDown();
   });
+
   $("#size-header").click(function() {
     $("#radio-buttons-size").slideToggle();
   });
+
   $("#sauce-header").click(function() {
     $("#radio-buttons-sauce").slideToggle();
   });
+
   $("#cheese-header").click(function() {
     $("#radio-buttons-cheese").slideToggle();
   });
+
   $("#meats-header").click(function() {
     $("#radio-buttons-meats").slideToggle();
   });
+
   $("#unmeats-header").click(function() {
     $("#radio-buttons-unmeats").slideToggle();
   });
+
+
   $("form.pizza").submit(function(event) {
     event.preventDefault();
+    const pizzaName = $("input#pizza-name-field").val();
     const pizzaSize = $("input:radio[name=pizza-size]:checked").val();
+    const pizzaSauce = $("input:radio[name=pizza-sauce]:checked").val();
+    const pizzaCheese = $("input:radio[name=pizza-cheese]:checked").val();
     const pizzaToppings = toppingsAssembler();
-    const clientPizza = new Pizza(pizzaSize, pizzaToppings);
+    const clientPizza = new Pizza(pizzaName, pizzaSize, pizzaSauce, pizzaCheese, pizzaToppings);
     $("#cart").slideDown();
-    $("#pizza-total").html(clientPizza.price());
+    clientPizza.addPrice();
+    clientPizza.addToCart();
+    newCartTotal = clientPizza.updateCartTotal(cart);
+    clientPizza.attachListeners();
+    updateCartTotal(newCartTotal);
+    cart = newCartTotal;
+    console.log(cart);
   });
+
 });
 
 function toppingsAssembler() {
   const pizzaToppings = [
-    $("input:checkbox[name=bacon]:checked").val(), 
-    $("input:checkbox[name=ground-beef]:checked").val(), 
-    $("input:checkbox[name=ham]:checked").val(), 
-    $("input:checkbox[name=italian-sausage]:checked").val(), 
-    $("input:checkbox[name=pepperoni]:checked").val(), 
-    $("input:checkbox[name=salami]:checked").val(), 
-    $("input:checkbox[name=garlic]:checked").val(), 
-    $("input:checkbox[name=green-bell-peppers]:checked").val(), 
-    $("input:checkbox[name=jalapenos]:checked").val(), 
-    $("input:checkbox[name=mushroom]:checked").val(), 
-    $("input:checkbox[name=olives]:checked").val(), 
-    $("input:checkbox[name=onion]:checked").val(), 
-    $("input:checkbox[name=pineapple]:checked").val(), 
+    $("input:checkbox[name=bacon]:checked").val(),
+    $("input:checkbox[name=ground-beef]:checked").val(),
+    $("input:checkbox[name=ham]:checked").val(),
+    $("input:checkbox[name=italian-sausage]:checked").val(),
+    $("input:checkbox[name=pepperoni]:checked").val(),
+    $("input:checkbox[name=salami]:checked").val(),
+    $("input:checkbox[name=garlic]:checked").val(),
+    $("input:checkbox[name=green-bell-peppers]:checked").val(),
+    $("input:checkbox[name=jalapenos]:checked").val(),
+    $("input:checkbox[name=mushroom]:checked").val(),
+    $("input:checkbox[name=olives]:checked").val(),
+    $("input:checkbox[name=onion]:checked").val(),
+    $("input:checkbox[name=pineapple]:checked").val(),
   ];
   const filteredPizzaToppings = undefinedFilter(pizzaToppings);
   return filteredPizzaToppings;
+};
+
+function updateCartTotal(newCartTotal) {
+  console.log("this should update the text with" + newCartTotal);
+  $("#cart-total").text(newCartTotal);
 };
